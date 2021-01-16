ee995350a5a25c4b0214addc3c5f7c4711a9b763
nirastor <62954081+nirastor@users.noreply.github.com>
2021-01-16 08:42:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,213 @@
+const inputJson = `{
+  "target_text": "jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon . jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon . jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon . jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon . jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon .",
+  "output": {
+      "NatDBH1-1 09_ target.docx": {
+          "1D_NAT_Mitxelena_Asier_NatDBH1_1": {
+              "matches": [
+
+                {
+                      "start_target": 1185,
+                      "match_length": 404,
+                      "text_target": " jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon .",
+                      "start_source": 1613,
+                      "text_source": " jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon .",
+                      "start_original": 1481,
+                      "end_original": 1957,
+                      "text_original": "Jaurlaritza eta Kontseilua lankidetzen antolatutakoren jardunaldiak izango dira , eta antolatzaileei garrantzitsua iruditzen zaie . « Bakoitzak bere bidea jorratu du ezagutzaren unibertsalizazioaren esparruan , baina badaude biok ditugun kezkak ere ; biok ikusi dugu gisa honetako foro bat antolatzeko beharra eta interesa » , azaldu du Urkijok . Dobaranen arabera , berriz , halako gaietan « inportantea » da erakunde publikoen eta gizarte eragileen arteko lankidetza egotea ."
+                  },
+                  {
+                    "start_target": 100,
+                    "match_length": 200,
+                    "text_target": " jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon .",
+                    "start_source": 1613,
+                    "text_source": " jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon .",
+                    "start_original": 1481,
+                    "end_original": 1957,
+                    "text_original": "Jaurlaritza eta Kontseilua lankidetzen antolatutakoren jardunaldiak izango dira , eta antolatzaileei garrantzitsua iruditzen zaie . « Bakoitzak bere bidea jorratu du ezagutzaren unibertsalizazioaren esparruan , baina badaude biok ditugun kezkak ere ; biok ikusi dugu gisa honetako foro bat antolatzeko beharra eta interesa » , azaldu du Urkijok . Dobaranen arabera , berriz , halako gaietan « inportantea » da erakunde publikoen eta gizarte eragileen arteko lankidetza egotea ."
+                }
+              ],
+              "total_match_length": 404,
+              "target_text": 2139,
+              "source": 2017,
+              "similarity": 18.887330528284245
+          },
+          "1D_NAT_Apalategi_Uxue_NatDBH1_1": {
+              "matches": [
+                  {
+                      "start_target": 1211,
+                      "match_length": 379,
+                      "text_target": " lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon . ",
+                      "start_source": 29,
+                      "text_source": " lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon . ",
+                      "start_original": 1508,
+                      "end_original": 1957,
+                      "text_original": "lankidetzen antolatutakoren jardunaldiak izango dira , eta antolatzaileei garrantzitsua iruditzen zaie . « Bakoitzak bere bidea jorratu du ezagutzaren unibertsalizazioaren esparruan , baina badaude biok ditugun kezkak ere ; biok ikusi dugu gisa honetako foro bat antolatzeko beharra eta interesa » , azaldu du Urkijok . Dobaranen arabera , berriz , halako gaietan « inportantea » da erakunde publikoen eta gizarte eragileen arteko lankidetza egotea ."
+                  }
+              ],
+              "total_match_length": 379,
+              "target_text": 2139,
+              "source": 2235,
+              "similarity": 17.71856007480131
+          },
+          "1D_NAT_Almorza_Aimar_NatDBH1_1": {
+              "matches": [
+                  {
+                      "start_target": 0,
+                      "match_length": 1594,
+                      "text_target": "bat agiri hori jaso proposatu neurri inguru gorrotxategi esan *edun ez izan erabat onartu edo baztertu testu bat eta hitz egin aukera eman *edun gu prest egon akordio iritsi beti ere balio ukan jende bizimodu nabarmen hobetu . bilera azpiazu ez *edun sakon erantzun eman gorrotxategi azaldu gaur bertan aurkeztu *edun dokumentu sailburu eta beraz ez *edun asti izan hura aztertu . halere sailburu interes aztertu *edun agindu *edun eta ahal bezain azkar erantzun *edun . bi neurri egon koalizio proposamen eragin *edun gastu 392 milioi euro handitu . multzo handi 152 milioi euro enplegu eta industria politiketa egin proposamen izan eta 65 milioi babes sozial eta giza eskubide bermatu neurri . gainera koalizio beste 55 milioi bideratu nahi ukan osasungintza nagusiki osakide lehen ar eta eriza lantalde indartu lan baldintza duindu eta covid - 19a txerto jarri baliabide areagotu . hala ere koronabirus intzidentzia - tasa gora egin jarraitu *edun eta 14 egun 100 . 000 biztanl 336 68 kasu izat iritsi izan astelehen 326 77 kasu gain . igoera hori hiru lurralde erregistratu izan 335 bizkaia 321 66 astelehen 331 87 gipuzkoa 327 71 astelehen eta 323 73 araba 315 86 astelehen . hiru jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon . lau ",
+                      "start_source": 0,
+                      "text_source": "bat agiri hori jaso proposatu neurri inguru gorrotxategi esan *edun ez izan erabat onartu edo baztertu testu bat eta hitz egin aukera eman *edun gu prest egon akordio iritsi beti ere balio ukan jende bizimodu nabarmen hobetu . bilera azpiazu ez *edun sakon erantzun eman gorrotxategi azaldu gaur bertan aurkeztu *edun dokumentu sailburu eta beraz ez *edun asti izan hura aztertu . halere sailburu interes aztertu *edun agindu *edun eta ahal bezain azkar erantzun *edun . bi neurri egon koalizio proposamen eragin *edun gastu 392 milioi euro handitu . multzo handi 152 milioi euro enplegu eta industria politiketa egin proposamen izan eta 65 milioi babes sozial eta giza eskubide bermatu neurri . gainera koalizio beste 55 milioi bideratu nahi ukan osasungintza nagusiki osakide lehen ar eta eriza lantalde indartu lan baldintza duindu eta covid - 19a txerto jarri baliabide areagotu . hala ere koronabirus intzidentzia - tasa gora egin jarraitu *edun eta 14 egun 100 . 000 biztanl 336 68 kasu izat iritsi izan astelehen 326 77 kasu gain . igoera hori hiru lurralde erregistratu izan 335 bizkaia 321 66 astelehen 331 87 gipuzkoa 327 71 astelehen eta 323 73 araba 315 86 astelehen . hiru jaurlaritza eta kontseilu lankidetu antolatutako jardunaldi izan izan eta antolatzaile garrantzitsu iruditu izan . bakoitz bera bide jorratu *edun ezagutza unibertsalizazio esparru baina egon bi *edun kezka ere bi ikusi *edun gisa hau foro bat antolatu behar eta interesa azaldu *edun urkijo . dobaran arabera berriz halako gai inportante izan erakunde publiko eta gizarte eragile arte lankidetza egon . lau ",
+                      "start_original": 0,
+                      "end_original": 1961,
+                      "text_original": "Bat Agiri horretan jasotako proposatutako neurrien inguruan , Gorrotxategik esan du ez dela erabat onartzeko edo baztertzeko testu bat , eta « hitz egiteko aukera » ematen duela : « Gu prest gaude akordioetara iristeko , beti ere balioko dutenak jendearen bizimodua nabarmen hobetzeko » . Bileran , Azpiazuk ez du « sakoneko erantzunik » eman ; Gorrotxategik azaldu gaur bertan aurkeztu diotela dokumentua sailburuari , eta , beraz , ez duela astirik izan hura aztertzeko . Halere , sailburuak « interesez » aztertuko duela agindu du , eta ahal bezain azkar erantzungo duela . Bi Neurriei dagokienez , koalizioaren proposamenek eragingo lukete gastua 392 , 9 milioi eurotan handitzea . Multzo handiena , 152 milioi euro , enplegu eta industria politiketan eginiko proposamenak dira , eta 65 , 9 milioi , babes sozialeko eta giza eskubideak bermatzeko neurrietara . Gainera , koalizioak beste 55 milioi bideratu nahi ditu osasungintzara ; nagusiki , Osakidetzako lehen arretako eta erizaintzako lantaldeak indartzeko , lan baldintzak « duintzeko » eta COVID - 19aren txertoa jartzeko baliabideak areagotzeko . Hala ere , koronabirusaren intzidentzia - tasak gora egiten jarraitzen du eta 14 egunetan 100 . 000 biztanleko 336 , 68 kasu izatera iritsi da , asteleheneko 326 , 77 kasuen gainetik . Igoera hori hiru lurraldeetan erregistratu da : 335 Bizkaian ( 321 , 66 astelehenean ) , 331 , 87 Gipuzkoan ( 327 , 71 astelehenean ) eta 323 , 73 Araban ( 315 , 86 astelehenean ) . Hiru Jaurlaritza eta Kontseilua lankidetzen antolatutakoren jardunaldiak izango dira , eta antolatzaileei garrantzitsua iruditzen zaie . « Bakoitzak bere bidea jorratu du ezagutzaren unibertsalizazioaren esparruan , baina badaude biok ditugun kezkak ere ; biok ikusi dugu gisa honetako foro bat antolatzeko beharra eta interesa » , azaldu du Urkijok . Dobaranen arabera , berriz , halako gaietan « inportantea » da erakunde publikoen eta gizarte eragileen arteko lankidetza egotea . Lau"
+                  }
+              ],
+              "total_match_length": 1594,
+              "target_text": 2139,
+              "source": 2274,
+              "similarity": 74.52080411407199
+          }
+      }
+  }
+}`
+
+// *** PREPAPRE DATA ***
+
+function createFragmentObject(start, end, isMatch) {
+  const fragmentObj = {
+    start,
+    end,
+    isMatch
+  };
+  return fragmentObj;
+}
+
+function getFragmentsBoundaries() {
+  const fragmentsBoundaries = {};
+  for (key in target) {
+    const fragments = target[key].matches.map((item) => createFragmentObject(
+      item.start_target,
+      item.start_target + item.match_length - 1,
+      true,
+    ));
+    
+    fragments.sort((a,b) => a.start - b.start);
+
+    // add first fragment if need
+    const firstSymbolIndex = fragments[0].start;
+    if (firstSymbolIndex !== 0) {
+      fragments.unshift(createFragmentObject(
+        0,
+        firstSymbolIndex - 1,
+        false,
+      ));
+    }
+
+    const targetString = input.target_text;
+    
+    // add last fragment if need
+    const lastSymbolIndex = fragments[fragments.length - 1].end
+    if (lastSymbolIndex < targetString.length) {
+      fragments.push(createFragmentObject(
+        lastSymbolIndex + 1,
+        targetString.length - 1,
+        false,
+      ));
+    }
+
+    // add internal fragments if need
+    for (let i = 0; i < fragments.length - 1; i += 1) {
+      const nextStart = fragments[i + 1].start;
+      const thisEnd = fragments[i].end
+      if (nextStart - thisEnd > 1) {
+        fragments.splice(i + 1, 0, createFragmentObject(
+          thisEnd + 1,
+          nextStart - 1,
+          false,
+        ));
+      }
+    }
+    
+    fragmentsBoundaries[key] = fragments;
+  }
+  return fragmentsBoundaries;
+}
+
+function getOneMarkedTextEl(text, fragments) {
+  const markupFullTextEl = document.createElement('div');
+    for (fragment of fragments) {
+      const textFragment = document.createElement('span');
+      textFragment.innerText = text.slice(fragment.start, fragment.end + 1);
+      if (fragment.isMatch) {
+        textFragment.classList.add('match');
+      }
+      markupFullTextEl.appendChild(textFragment);
+    }
+  return markupFullTextEl;
+}
+
+function getAllMarkedTextsEl(text, fragmentsBoundaries) {
+  const markedTextsEl = {};
+  for (key in fragmentsBoundaries) {
+    markedTextsEl[key] = getOneMarkedTextEl(text, fragmentsBoundaries[key]);
+  }
+  return markedTextsEl;
+}
+
+// *** INTERFACE ***
+
+function showText(id) {
+  if (id) {
+    targetTextEl.innerHTML = '';
+    targetTextEl.appendChild(markedTextsEl[id]);
+  } else {
+    targetTextEl.innerText = input.target_text;
+  }
+}
+
+function clickHandler(elem) {
+  if (elem.classList.contains('selected')) {
+    elem.classList.remove('selected');
+    showText();
+  } else {
+    documentsList.forEach((el) => el.classList.remove('selected'));
+    elem.classList.add('selected');
+    showText(elem.id);
+  }
+}
+
+function createDocumentList() {
+  for (key in target) {
+    const newDocumentItem = document.createElement('div');
+    newDocumentItem.id = key;
+    const percent = Math.round(target[key].similarity);
+    newDocumentItem.innerHTML = `
+      <div class="item-title">${key}</div>
+      <div class="item-percent">${percent}%</div>
+    `
+    newDocumentItem.classList.add('documents-list-item');
+    newDocumentItem.addEventListener('click', (e) => {
+      e.preventDefault();
+      clickHandler(e.currentTarget);
+    })
+    documentsListEl.appendChild(newDocumentItem);
+    documentsList.push(newDocumentItem);
+  }
+}
+
+// *** MAIN ***
+
+// read input
+const input = JSON.parse(inputJson);
+const targetDoc = 'NatDBH1-1 09_ target.docx'
+const target = input.output[targetDoc];
+
+// prepare data
+const fragmentsBoundaries = getFragmentsBoundaries();
+const markedTextsEl = getAllMarkedTextsEl(input.target_text, fragmentsBoundaries);
+
+// init interface
+const targetTextEl = document.querySelector('.target-text');
+const documentsListEl = document.querySelector('.documents-list');
+const documentsList = [];
+createDocumentList();
